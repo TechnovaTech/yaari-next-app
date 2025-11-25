e74bb120df2b6e5f74e942064029d1c72457714d
kakkadpriyansh <kakkadpriyansh@gmail.com>
2025-11-25 16:50:16 +0530

    SMS

--- yarri admin panel/lib/sms-service.ts
+++ yarri admin panel/lib/sms-service.ts
@@ -24,7 +24,7 @@ class SMSService {
       password: process.env.GUPSHUP_PASSWORD || '*v$4g3My',
       baseUrl: process.env.GUPSHUP_BASE_URL || 'https://enterprise.smsgupshup.com',
       mask: process.env.GUPSHUP_MASK || 'YAARIP',
-      dltTemplateId: process.env.DLT_TEMPLATE_ID || '1707176166167642496',
+      dltTemplateId: process.env.DLT_TEMPLATE_ID || '1707176166167640000',
       dltEntityId: process.env.DLT_ENTITY_ID || undefined,
     };
   }
