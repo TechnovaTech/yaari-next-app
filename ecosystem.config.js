fdc32d88abdeec37694829fc412bed082c161d3f
kakkadpriyansh <kakkadpriyansh@gmail.com>
2025-11-27 11:54:23 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,8 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 3000,
+      TRUECALLER_CLIENT_ID: 'fn_yohgvr75otxdy6eqursnetjuhk8b8xqdqzvahurs'
     }
   }]
 }
